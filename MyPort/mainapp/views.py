c81fc8612da15abba3c3273c6ef387808b8e1b2b
horoyoii <demonic3540@naver.com>
2019-02-12 18:08:43 +0900

    setup
    setup

---
+++ MyPort/mainapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'mainapp/index.html', {})
+
+def profile(request):
+    return render(request, 'mainapp/profile.html', {})
